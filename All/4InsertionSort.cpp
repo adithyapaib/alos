0ad49cca998346d13e2012dc4e7e2ef4c06b915b
Adithya Pai B <paiadithya26@gmail.com>
2023-09-03 15:03:58 +0530

    Updated Insertion Sort

--- All/4InsertionSort.cpp
+++ All/4InsertionSort.cpp
@@ -5,11 +5,8 @@ void insertion(int arr[], int n){
     for(int i = 0,j=0;i<n;i++,j=i)
         while(j>0 && arr[j-1] > arr[j])
                swap(arr[j],arr[j--]);
-}
 
-
-void printArray(int arr[], int size){
-     for(int i = 0;i<size;i++)
+    for(int i = 0;i<n;i++)
         cout<<arr[i]<<endl;
 }
 
@@ -18,7 +15,5 @@ int main()
 	int arr[] = { 1,2,3,4,5,604, 34, 25, 12, 22, 11, 90 };
 	int N = sizeof(arr) / sizeof(arr[0]);
 	insertion(arr, N);
-	cout << "Sorted array: \n";
-	printArray(arr, N);
 	return 0;
 }
