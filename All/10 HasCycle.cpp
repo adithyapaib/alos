7fed5ba088382485e51b60bbd63d0fa20ea04dbd
Adithya Pai B <paiadithya26@gmail.com>
2023-11-14 20:39:04 +0530

    Update 10 HasCycle.cpp

--- All/10 HasCycle.cpp
+++ All/10 HasCycle.cpp
@@ -7,36 +7,32 @@ int graph[10][10] = {0};
 int numNodes, numEdges; 
 int path[10], pathIndex = 0;
 
-
-
-
 void dfs(int node, int parent) {
-
   visited[node] = true;
   path[pathIndex++] = node;
 
   for(int i = 0; i < numNodes; i++) {
     if(graph[node][i] == 1 && !visited[i]) {
       dfs(i, node); 
-      return;
     }
     else if(graph[node][i] == 1 && i != parent) {
-     hasCycle = true;
-     cout << "Cycle: ";
-    for (int i = 0; i < pathIndex; i++) 
-        cout << path[i] << "-> ";
-      return;
+      hasCycle = true;
+      cout << "Cycle: ";
+      for (int j = 0; j < pathIndex; j++) {
+        cout << path[j] << "-> ";
+      }
+      cout << path[i] << endl;
+      exit(0);
     }
   }
   pathIndex--;
 }
 
 int main() {
-
   cout << "Enter number of nodes and edges: ";
   cin >> numNodes >> numEdges;
 
-  cout << "Enter edges:\n";
+  cout << "Enter edges:" << endl;
   for(int i = 0; i < numEdges; i++) {
     int u, v;
     cin >> u >> v;
@@ -44,16 +40,13 @@ int main() {
     graph[v][u] = 1;
   }
 
-  
   for(int i = 0; i < numNodes; i++) {
     if(!visited[i])
       dfs(i, -1); 
   }
 
-  if(hasCycle) 
-    cout << "Graph contains cycle";
-  else
-    cout << "Graph doesn't contain cycle";
+  if(!hasCycle)
+    cout << "Graph doesn't contain cycle" << endl;
 
   return 0;
 }
